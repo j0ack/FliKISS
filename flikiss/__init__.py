4b1ca6931ce067239eede32727616cc5a29bd11c
TROUVERIE Joachim <joachim.trouverie@joacodepel.tk>
2014-10-01 09:13:48 +0000

    Skeleton

---
+++ flikiss/__init__.py
@@ -0,0 +1,13 @@
+#! /usr/bin/env python
+#-*- coding: utf-8 -*-
+# vim:set shiftwidth=4 tabstop=4 expandtab textwidth=79:
+
+"""
+    FliKISS 
+    ~~~~~~~
+
+    A personal wiki engine inspired from BlazeKISS
+    based on flat files written in Markdown
+"""
+
+__author__ = u'TROUVERIE Joachim'
---
+++ flikiss/models.py
@@ -0,0 +1,10 @@
+#! /usr/bin/env python
+#-*- coding: utf-8 -*-
+# vim:set shiftwidth=4 tabstop=4 expandtab textwidth=79:
+
+"""
+    Models for FliKISS app
+    ~~~~~~~~~~~~~~~~~~~~~~
+"""
+
+__author__ = u'TROUVERIE Joachim'
